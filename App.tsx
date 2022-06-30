e0d4f3855bb639984a55175748d2f44dc33aea43
azanniel <leo.azannielttt@gmail.com>
2022-06-29 20:08:37 -0400

    add chart of the expansives

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
-import 'react-native-gesture-handler';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import 'react-native-gesture-handler';
 
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold, useFonts
+} from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ThemeProvider } from 'styled-components';
-import { NavigationContainer } from '@react-navigation/native';
-import * as SplashScreen from 'expo-splash-screen';
-import {
-  useFonts,
-  Poppins_400Regular,
-  Poppins_500Medium,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
 import { AppRoutes } from './src/routes/app.routes';
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,13 +1,14 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import { Platform } from 'react-native';
 import { useTheme } from 'styled-components/native';
-import { MaterialIcons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 
 export function AppRoutes() {
   const theme = useTheme();
@@ -45,7 +46,7 @@ export function AppRoutes() {
 
       <Screen
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: ({ size, color }) => 
             <MaterialIcons name='pie-chart' size={size} color={color} />
