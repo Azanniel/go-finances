1949daada9d2a54546f86d0b86ccc8a7c69c9b2b
azanniel <leo.azannielttt@gmail.com>
2022-11-19 13:21:20 -0400

    improve my suit test

--- src/__test__/screens/profile.spec.tsx
+++ src/__test__/screens/profile.spec.tsx
@@ -3,13 +3,31 @@ import { render } from '@testing-library/react-native';
 
 import { Profile } from '../../screens/Profile'
 
-describe('Profile screen', () => {
+describe('Profile', () => {
 
-  test('Check if show correctly user input name placeholder', () => {
-    const { getByPlaceholderText } = render(<Profile />)
+  it('should have placeholder correctly in user name input', () => {
+    const { getByPlaceholderText } = render(<Profile />);
 
     const inputName = getByPlaceholderText('Nome');
     
     expect(inputName).toBeTruthy();
+  });
+
+  it('should be load the user data', () => {
+    const { getByTestId } = render(<Profile />);
+    
+    const inputName = getByTestId('input-name');
+    const inputSurname = getByTestId('input-surname');
+
+    expect(inputName.props.value.trim()).toEqual('Leandro');
+    expect(inputSurname.props.value.trim()).toEqual('Azanniel');
+  });
+
+  it('should exist title correctly', () => {
+    const { getByTestId } = render(<Profile />);
+
+    const textTitle = getByTestId('text-title');
+
+    expect(textTitle.props.children).toContain('Perfil');
   })
 })
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -3,15 +3,21 @@ import { View, Text, TextInput, Button } from 'react-native';
 export function Profile() {
   return (
     <View>
-      <Text>Perfil</Text>
+      <Text testID='text-title'>
+        Perfil
+      </Text>
 
       <TextInput
+        testID='input-name'
         placeholder='Nome'
         autoCorrect={false}
+        value='Leandro'
       />
 
       <TextInput
+        testID='input-surname'
         placeholder='Sobrenome'
+        value='Azanniel'
       />
 
       <Button
